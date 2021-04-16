040a44599936a438abe273340f4eb40a262a6a80
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-16 14:33:51 +0800

    juice: build perf

--- device.mk
+++ device.mk
@@ -89,6 +89,12 @@ PRODUCT_PACKAGES += \
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Power
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.perf@2.0 \
+    vendor.qti.hardware.perf@2.1 \
+    vendor.qti.hardware.perf@2.2
+
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
