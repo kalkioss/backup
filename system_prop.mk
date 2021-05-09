c32bf824f60ee45032115c2a658829f91b7e97c1
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-05-09 09:22:23 +0800

    juice: add stock bluetooth props and scrolling optimization

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,13 @@
+# Bluetooth
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.vendor.bt.a2dp.aac_whitelist=false \
+    persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
+    persist.vendor.btstack.enable.splita2dp=true \
+    persist.vendor.btstack.enable.twsplus=true \
+    persist.vendor.btstack.enable.twsplussho=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
+    vendor.bluetooth.soc=cherokee
+
 # IMS
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
@@ -7,3 +17,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.radio.force_ltd_sys_ind=1 \
     persist.vendor.radio.voice_on_lte=1 \
     persist.vendor.radio.calls.on.ims=1
+
+# Scroll optimization
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.perf.scroll_opt=true
