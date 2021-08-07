f67f9298d1e1c1bca06e6a2715f566dff70a2add
Alex Winkowski <dereference23@outlook.com>
2021-08-07 18:18:01 +0530

    juice: Enable IORap app launch prefetching
    Co-Authored-By: Avinash Dwivedi <kalkioss10@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -6,6 +6,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     drm.service.enabled=true
 
+# IORap
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.iorapd.enable=true \
+    persist.device_config.runtime_native_boot.iorap_readahead_enable=true \
+    persist.device_config.runtime_native_boot.iorap_perfetto_enable=true
+
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.media_vol_steps=25 \
