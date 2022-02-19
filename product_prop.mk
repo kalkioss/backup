afe5f9fceef064526a4934bd547b0dcbd2bc8463
Adrian DC <radian.dc@gmail.com>
2022-02-19 20:31:52 +0530

    juice: Disable EGL buffer_age extension support

--- product_prop.mk
+++ product_prop.mk
@@ -12,7 +12,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiagl \
     debug.renderengine.backend=skiaglthreaded \
     renderthread.skia.reduceopstasksplitting=true \
-    vendor.display.disable_rotator_downscale=1
+    vendor.display.disable_rotator_downscale=1 \
+    debug.hwui.use_buffer_age=false
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
